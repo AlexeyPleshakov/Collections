33182106d409c5e70412c7114828795281578ba2
Алексей Плешаков <pleshakov-aleks@yandex.ru>
2024-06-12 01:59:07 +0400

    Добавьте файлы проекта.

---
+++ Collections/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace Collections
+{
+    class Program
+    {
+        List<string> List = new List<string>();
+        LinkedList<string> LinkedList = new LinkedList<string>();
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            string text = File.ReadAllText(args[0]);
+
+            var watchTwo = Stopwatch.StartNew();
+
+            program.List.Add(text);
+            Console.WriteLine($"Вставка в List: {watchTwo.Elapsed.TotalMilliseconds}");
+            Console.WriteLine();
+
+            watchTwo.Restart();
+
+            program.LinkedList.AddFirst(text);
+            Console.WriteLine($"Вставка в LinkedList: {watchTwo.Elapsed.TotalMilliseconds}");
+        }
+    }
+}
---
+++ Task two/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Как можно было еще решить эту задачу?
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = File.ReadAllText(args[0]);
+        string noPunctuationText = new string(text.ToLower().Where(c => !char.IsPunctuation(c)).ToArray());
+        char[] delimiters = new char[] { ' ', '\r', '\n' };
+        string[] strings = noPunctuationText.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+        Dictionary<string,int> wordFrequency = new Dictionary<string,int>();
+        HashSet<string> uniqueWords = new HashSet<string>(strings);
+        foreach (string s in strings)
+        {
+            if (wordFrequency.ContainsKey(s))
+            {
+                wordFrequency[s]++;
+            }
+            else
+            {
+                wordFrequency[s] = 1;
+            }
+        }
+        Console.WriteLine("10 самых частых слов в книге:");
+        foreach (var word in wordFrequency.OrderByDescending(x => x.Value).Take(10))
+        {
+            Console.WriteLine($"{word.Key} - {word.Value} раз(а)");
+        }
+        Console.WriteLine($"Количество уникальных слов: {uniqueWords.Count}");
+    }
+}
